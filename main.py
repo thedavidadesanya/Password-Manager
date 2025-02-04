6f26e8263d668549648720e0b043e738a805048d
David Adesanya <davidace2004@gmail.com>
2025-02-03 21:32:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -25,9 +25,22 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password) #helps to copy the password string
 
-
+# -------------------------- FIND PASSWORD ------------------------------- #
 def search_password():
-    pass
+    website = website_entry.get()
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email} \n Password: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists!")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_to_file():
@@ -40,17 +53,16 @@ def save_to_file():
                           }
                 }
 
-    if len(website_entry.get()) == 0 or len(password_entry.get()) == 0:
+    if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooops", message="Please make sure you didn't leave any field empyty")
     else:
         try:
             with open("data.json", mode="r") as file:
                 #Reading old data
                 data = json.load(file)
-                #Updating old data with new data
         except FileNotFoundError:
             with open("data.json", mode="w") as file:
-                data.update(new_data, file, indent=4)
+                json.dump(new_data, file, indent=4)
         else:
             #updating old data with new data
             data.update(new_data)
@@ -86,24 +98,24 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry=Entry(width=35,)
+email_entry=Entry(width=21)
 email_entry.grid(row=2, column=1)
 email_entry.insert(0, "david@gmail.com")
-password_entry = Entry(width=35)
+password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=35, command=save_to_file)
+add_button = Button(text="Add", width=21, command=save_to_file)
 add_button.grid(row=4, column=1, columnspan=2)
 
-search_button = Button(text="Search", command= search_password)
-search_button.grid(row=1, column=2)
+search_button = Button(text="Search",width=13, command= search_password)
+search_button.grid(row=1, column=2, columnspan=2)
 
 
 
